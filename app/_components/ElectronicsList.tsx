6bbaf805c02aeb45eb87e2f97e5bddcddd0c9d05
Andrei Vilnoiu <Vilnoiuandrei@gmail.com>
2024-12-13 18:41:19 +0200

    electronic list

--- app/_components/ElectronicsList.tsx
+++ app/_components/ElectronicsList.tsx
@@ -1,3 +1,51 @@
-export default function ElectronicsList() {
-  return <div></div>;
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import ProductItem from "../_components/Product";
+import Loader from "@/components/Loader";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+const fetchElectronics = async (): Promise<Product[]> => {
+  const res = await fetch(
+    "https://fakestoreapi.com/products/category/electronics"
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch electronics");
+  }
+
+  return res.json();
+};
+
+export default function ElectronicsList({}) {
+  const { data, isLoading, isError } = useQuery<Product[]>({
+    queryKey: ["electronics"],
+    queryFn: fetchElectronics,
+  });
+
+  if (isLoading) return <Loader />;
+  if (isError) return <p>Error loading electronics.</p>;
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-6">Electronics</h1>
+      <ul className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data?.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </ul>
+    </div>
+  );
 }
